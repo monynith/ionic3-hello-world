0966c984d8d8d8fab9374037a843d1b504add63f
monynith <monynith@gmail.com>
2018-09-04 16:24:30 +0700

    hello world

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.http.get("https://ionic3-example.herokuapp.com").subscribe(data => {
+      alert(data["message"]);
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
